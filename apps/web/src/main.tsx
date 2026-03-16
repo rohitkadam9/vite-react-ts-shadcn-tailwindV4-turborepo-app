be9a9a573ac84625e96fde2e864c994a01651fc9
Rohit kadam <me.rohitkadam@gmail.com>
2026-03-17 01:43:57 +0530

    Refactor code structure for improved readability and maintainability

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,24 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import { Button } from '@repo/ui/components/ui/button'
 
 const App = () => (
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
-    </div>
+    <Button>Click</Button>
   </div>
 );
 
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,3 @@
 // components
 export * from "./components";
+export * from "./lib/utils";
