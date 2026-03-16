b7b221b2d51ec68a801f4de8c967c340c3922589
Rohit kadam <me.rohitkadam@gmail.com>
2026-03-17 01:59:32 +0530

    Refactor UI components: replace Button and Header with LoginForm; update component exports and add new components

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import { Button } from '@repo/ui/components/ui/button'
+import { LoginForm } from "@repo/ui/components/ui-custom/login-form"
 
 const App = () => (
-  <div>
-    <Button>Click</Button>
-  </div>
+  <>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  </>
 );
 
 createRoot(document.getElementById("app")!).render(<App />);
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,9 @@
-export * from "./header";
-export * from "./counter";
-export * from "./ui/button";
+export * from "@repo/ui/components/shadcn/button";
+export * from "@repo/ui/components/shadcn/card";
+export * from "@repo/ui/components/shadcn/field";
+export * from "@repo/ui/components/shadcn/input";
+export * from "@repo/ui/components/shadcn/label";
+export * from "@repo/ui/components/shadcn/separator";
+
+// UI CUSTOM
+export * from "@repo/ui/components/ui-custom/login-form";
